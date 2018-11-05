cc2d742a1f4ba956341f5ca3e2c225ac03623d5b
JureLokovsek <lokovsek.jure@gmail.com>
2018-11-05 21:35:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jure_lokovsek/constraintlayout/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.jure_lokovsek.constraintlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.Placeholder;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+
+public class MainActivity extends Activity {
+
+    private Placeholder mPlaceholder;
+    private ConstraintLayout mLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPlaceholder = findViewById(R.id.placeholder);
+        mLayout = findViewById(R.id.layout);
+
+
+    }
+
+    public void click(View view){
+        TransitionManager.beginDelayedTransition(mLayout);
+        mPlaceholder.setContentId(view.getId());
+    }
+
+
+}
